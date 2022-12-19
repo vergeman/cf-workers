1a16b01d77f2412f0a7377d4e7d67fb3f8f8a1e3
vergeman <alan.verga@gmail.com>
2022-12-19 01:01:46 -0600

    generateIcon(): create svg with visit counts

--- kv-image-counter/src/index.ts
+++ kv-image-counter/src/index.ts
@@ -19,6 +19,12 @@ export interface Env {
   // MY_BUCKET: R2Bucket;
 }
 
+/*
+ * This is a hit-counter using cf workers and KV
+ * hopefully this works from README.md
+ */
+
+
 async function incrementKey(NAMESPACE:KVNamespace, key:string): Promise<number> {
     let count = await NAMESPACE.get(key) || "0";
     let value = parseInt(count);
@@ -27,14 +33,43 @@ async function incrementKey(NAMESPACE:KVNamespace, key:string): Promise<number> 
 }
 
 
+/*
+ * ripped off from https://badgen.net/
+ * e.g. https://badgen.net/badge/icon/visitor%201000/red?icon=codebeat&label
+ */
+
+function generateIcon(count: number) {
+
+    let svg = `
+    <svg width="97" height="20" viewBox="0 0 970 200" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" role="img" aria-label="visit ${count}">
+  <title>visit ${count}</title>
+  <linearGradient id="a" x2="0" y2="100%">
+    <stop offset="0" stop-opacity=".1" stop-color="#EEE"/>
+    <stop offset="1" stop-opacity=".1"/>
+  </linearGradient>
+  <mask id="m"><rect width="970" height="200" rx="30" fill="#FFF"/></mask>
+  <g mask="url(#m)">
+    <rect width="212" height="200" fill="#555"/>
+    <rect width="758" height="200" fill="#E43" x="212"/>
+    <rect width="970" height="200" fill="url(#a)"/>
+  </g>
+  <g aria-hidden="true" fill="#fff" text-anchor="start" font-family="Verdana,DejaVu Sans,sans-serif" font-size="110">
+    <text x="172" y="148" textLength="0" fill="#000" opacity="0.25"></text>
+    <text x="162" y="138" textLength="0"></text>
+    <text x="267" y="148" textLength="658" fill="#000" opacity="0.25">Visit ${count}</text>
+    <text x="257" y="138" textLength="658">Visit ${count}</text>
+  </g>
+  <image x="40" y="35" width="130" height="130" xlink:href="data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjZmZmIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzMzEgMzMxIj48cGF0aCBjbGFzcz0ic29sby1sb2dvLTAiIGQ9Ik03NC42IDExNS44bDM2LjEgNzUuMyA2Ni0xNTAuMyA3Mi44IDE3OS43IDIzLjItNTIuMkgzMzF2LTIuOEMzMzEgNzQuMSAyNTYuOSAwIDE2NS41IDBTMCA3NC4xIDAgMTY1LjV2Mi44aDQ3LjhsMjYuOC01Mi41eiIvPjxwYXRoIGNsYXNzPSJzb2xvLWxvZ28tMCIgZD0iTTI0OSAyNjIuN2wtNzMuNC0xNDEuOS02NS4zIDExNy41TDc0IDE1NC42bC0xNy43IDM0LjhIMS43QzEzLjMgMjY5LjUgODIuMiAzMzEgMTY1LjUgMzMxczE1Mi4zLTYxLjYgMTYzLjgtMTQxLjdoLTQ1LjZMMjQ5IDI2Mi43eiIvPjwvc3ZnPgo="/>
+</svg>`;
+    return svg;
+}
+
 export default {
   async fetch(
     request: Request,
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-
-      const headers = {"content-type": "application/json"};
 
       //headers for "visitor" and total visits (TOTAL_COUNT)
       const key = [
@@ -48,6 +83,7 @@ export default {
 
       const totalKey = "TOTAL_COUNT";
 
+      //increment counts per "visitor" and total
       let value = await incrementKey(env.IMAGE_COUNTER, key)
       let totalValue = await incrementKey(env.IMAGE_COUNTER, totalKey);
 
@@ -60,6 +96,10 @@ export default {
           env.IMAGE_COUNTER.put(totalKey, totalValue.toString())
       ]);
 
-      return new Response(JSON.stringify(res), {headers});
+      console.log(res);
+
+      const headers = { "content-type": "image/svg+xml" };
+      const svg = generateIcon(totalValue);
+      return new Response(svg, {headers});
   },
 };
